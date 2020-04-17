690f06843cda1e42fa7eace15dc5efff33cdf36a
Ben Venker <bvemails@gmail.com>
2020-04-17 17:57:57 -0500

    pull out actions into their own file

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ import Total from "./components/Total";
 
 const App = (props) => {
   console.log("props!", props);
-  const removeFeature = (item) => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = (item) => {
-    // dipsatch an action here to add an item
-  };
-
   return (
     <div className="boxes">
       <div className="box">
